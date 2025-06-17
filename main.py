bb7da87010d3bd5d4b52a3d130fc1a3413fb16bd
Özlem Nur Duman <ozlemnduman@stu.aydin.edu.tr>
2025-06-17 19:26:58 +0300

    unused module 'main.py' is removed

--- main.py
+++
@@ -1,9 +0,0 @@
-from preprocessing import preprocess_data
-from models.stacking_model import run_stacking_model
-from models.voting_model import run_voting_model
-import pandas as pd
-
-def main():
-    data_source = "C:\Users\Özlem Nur\Desktop\data.csv"
-    df, X_train, X_test, y_train, y_test = preprocess_data(data_source)
-
